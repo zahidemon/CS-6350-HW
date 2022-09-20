b9bb6f188f5770f7c124ba988289bbf6829fbaeb
Abu Zahid Bin Aziz <abuzahid.cse@gmail.com>
2022-09-20 11:41:10 -0600

    added data utils

--- DecisionTree/utils/data.py
+++ DecisionTree/utils/data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas a pd
 
 
 def get_attributes_and_labels(filename):
@@ -13,3 +14,31 @@ def get_attributes_and_labels(filename):
     y = np.array(y)
 
     return x, y
+
+
+def create_dataframe(filename):
+    data = {
+        "buying": [],
+        "maint": [],
+        "doors": [],
+        "persons": [],
+        "lug_boot": [],
+        "safety": [],
+        "labels": []
+    }
+    data_keys = list(data.keys())
+    with open(filename, "r") as f:
+        for line in f:
+            row = line.strip().split(",")
+            for i in range(len(row)):
+                data.get(data_keys[i]).append(row[i])
+
+    return pd.data
+
+
+def filter_dataframe(dataframe, column, value):
+    return dataframe[dataframe[column] == value]
+
+
+if __name__ == "__main__":
+    print(create_dataframe("../../Data/Car/train.csv"))
